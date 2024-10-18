078e51e79bf6691a493eceed249d8b321acad2e2
SamZec <samzec4@gmail.com>
2024-10-18 14:07:58 +0000

    Update oauth2 login

--- api/v1/views/oauth2.py
+++ api/v1/views/oauth2.py
@@ -9,7 +9,7 @@ from flask import redirect, request, abort, jsonify
 
 
 APP_ID = config('APP_ID')
-REDIRECT_URL = 'http://localhost:4321/response'
+REDIRECT_URL = config('REDIRECT_URL')
 APP_SECRET = config('APP_SECRET')
 ACCESS_TOKEN = config('FB_ACCESS_TOKEN')
 
@@ -20,11 +20,9 @@ ACCESS_TOKEN = config('FB_ACCESS_TOKEN')
 def oauth2():
     """Oauth2 third party login"""
     try:
-        APP_ID = config('APP_ID')
-        REDIRECT_URL = 'http://localhost:4321/response'
         FB_URL = 'https://www.facebook.com/v21.0/dialog/oauth?'
         CLIENT_ID_ARG = f'client_id={APP_ID}&'
-        REDIRECT_URL_ARG = f'redirect_uri={REDIRECT_URL}&'
+        REDIRECT_URL_ARG = f'redirect_uri={REDIRECT_URL}'
         return jsonify({
             'msg': 'use link to authorize log in',
             'link': FB_URL + CLIENT_ID_ARG + REDIRECT_URL_ARG}), 200
@@ -47,7 +45,28 @@ def response():
     if res.json().get('access_token'):
         FB_URL = 'https://graph.facebook.com/debug_token?'
         INPUT_TOKEN_ARG = f"input_token={res.json().get('access_token')}&"
-        ACCESS_TOKEN_ARG = f"access_token={config('ACCESS_TOKEN')}"
-        veryfy = requests.get(FB_URL + INPUT_TOKEN_ARG + ACCESS_TOKEN_ARG)
-        return veryfy.json()
-    return res.json()
+        ACCESS_TOKEN_ARG = f"access_token={ACCESS_TOKEN}"
+
+        verify = requests.get(FB_URL + INPUT_TOKEN_ARG + ACCESS_TOKEN_ARG)
+        if verify.json().get('data').get('is_valid'):
+            from models.auth import Auth
+            from flask_jwt_extended import create_access_token
+            auth = Auth()
+            graph = facebook.GraphAPI(ACCESS_TOKEN)
+            user = graph.get_object(verify.json().get('data').get('user_id'),
+                    fields='first_name, last_name, email')
+            if not auth.valid_login(user['email'], user['id']):
+                auth_user = auth.register_user(first_name=user['first_name'],
+                        last_name=user['last_name'], email=user['email'],
+                        password=user['id'])
+
+            session_id = auth.create_session(user['email'])
+            if auth.get_user_from_session_id(session_id):
+                del user['id']
+                user['session_id'] = session_id
+                token = create_access_token(identity=user)
+                return jsonify({
+                    'msg': 'loggin successful',
+                    'access_token': token}), 200
+
+    return abort(401, res.json().get('error').get('message')), 401
